835127d88270353b124291fdb841d77085c883df
Cody Hodges <hodges.cody00@gmail.com>
2022-03-26 17:54:54 -0500

    Update

--- js/page.js
+++ js/page.js
@@ -9,9 +9,16 @@
 // ADD RESUME IMG'S !!!!!!!!!!!!!!!!!!
 
 // RESUME LINK FUNCTIONALITY
-let aboutUsContent = `<h1>Hello</h1>`
+let aboutUsContent = `
+<div id="about-content" >
+<h1>Hello!</h1><br>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda, at aut beatae consequuntur cumque deleniti distinctio eligendi eos eveniet, facere fuga ipsum laborum libero maxime molestiae natus nihil quasi quibusdam quo rem similique temporibus veniam vitae voluptas voluptates voluptatibus?</p>
+
+</div>
+`
 
 function showAbout() {
+    $('body').css('background-color', 'grey');
     aboutPage.innerHTML = aboutUsContent;
     homeBTN.innerHTML = `<a href="#" onclick="location.reload()">Return Home</a>`
 }
@@ -32,18 +39,19 @@ let homeBTN = document.querySelector('#returnHome');
 // CONTACT LINK FUNCTIONALITY
 let supportersContent =
     `<div id="contact-box">
-<div id="photo"><img src="../assets/Headshot.png"></div>
+<!--<div id="photo"><img src="../assets/Headshot.png"></div>-->
      <div id="contact-info">
-     <h5>CONTACT</h5><br>
+     <h5>CONTACT</h5><br><br>
      <p>Phone  :  971-707-1419</p><br>
      <p>Email  :  Chodges00@gmail.com</p><br>
      <p>Github  :  @CHodges00</p><br>
      <p>LinkedIn  :  Cody Hodges</p>
+     <br><br>
      </div> 
       </div>`
 
 function showSupporters() {
-    $('body').toggleClass('bg')
+    $('body').css('background-color', 'grey');
     supportersPage.innerHTML = supportersContent;
     homeBTN.innerHTML = `<a href="#" onclick="location.reload()">Return Home</a>`
 }
