e4d86781963211dd90a4c07a0ec448b7069a017c
Cody Hodges <hodges.cody00@gmail.com>
2022-03-25 23:54:16 -0500

    Links

--- js/page.js
+++ js/page.js
@@ -1,5 +1,5 @@
 
-let aboutUsContent = `<img src="../assets/Resume6.jpg"><br><img src="../assets/Resume2.pdf">`
+let aboutUsContent = `<img src="../assets/Resume8.pdf"><br><img src="../assets/Resume2.pdf">`
 
 function showAbout() {
     aboutPage.innerHTML = aboutUsContent;
@@ -21,9 +21,20 @@ let homeBTN = document.querySelector('#returnHome');
 
 
 
-let supportersContent = `<h5>CONTACT</h5><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At debitis dolorum enim, excepturi fugit maxime nemo reiciendis rerum totam! Voluptas.</p>`
+let supportersContent =
+    `<div id="contact-box">
+<div id="photo"><img src="../assets/Headshot.png"></div>
+     <div id="contact-info">
+     <h5>CONTACT</h5><br>
+     <p>Phone  :  971-707-1419</p><br>
+     <p>Email  :  Chodges00@gmail.com</p><br>
+     <p>Github  :  @CHodges00</p><br>
+     <p>LinkedIn  :  Cody Hodges</p>
+     </div> 
+      </div>`
 
 function showSupporters() {
+    $('body').toggleClass('bg')
     supportersPage.innerHTML = supportersContent;
     homeBTN.innerHTML = `<a href="#" onclick="location.reload()">Return Home</a>`
 }
