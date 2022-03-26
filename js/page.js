4480d6a1bfcd265312f9cd1626e01301e3e577a1
Cody Hodges <hodges.cody00@gmail.com>
2022-03-25 23:22:52 -0500

    Links

--- js/page.js
+++ js/page.js
@@ -1,5 +1,5 @@
 
-let aboutUsContent = `<img src="../assets/Resume1.pdf"><br><img src="../assets/Resume2.pdf">`
+let aboutUsContent = `<img src="../assets/Resume6.jpg"><br><img src="../assets/Resume2.pdf">`
 
 function showAbout() {
     aboutPage.innerHTML = aboutUsContent;
@@ -13,3 +13,23 @@ aboutBTN.addEventListener('click', showAbout);
 
 let aboutPage = document.querySelector('#main-page');
 let homeBTN = document.querySelector('#returnHome');
+
+
+
+
+
+
+
+
+let supportersContent = `<h5>CONTACT</h5><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At debitis dolorum enim, excepturi fugit maxime nemo reiciendis rerum totam! Voluptas.</p>`
+
+function showSupporters() {
+    supportersPage.innerHTML = supportersContent;
+    homeBTN.innerHTML = `<a href="#" onclick="location.reload()">Return Home</a>`
+}
+
+
+let supportersBtn = document.querySelector('#supporters');
+let supportersPage = document.querySelector('#main-page');
+
+supportersBtn.addEventListener('click', showSupporters);
